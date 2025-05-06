c99bafbf700f470a05dd7d42038ca44ca9402e8d
TheFashola <aaofashola@gmail.com>
2025-05-06 14:30:28 +1000

    update

--- Games/Pacman/readme.md
+++
@@ -1,1 +0,0 @@
-To run, execute: gradle clean build run
--- Games/Pacman/src/main/java/pacman/model/entity/staticentity/collectable/PelletComponent.java
+++
@@ -1,9 +0,0 @@
-package pacman.model.entity.staticentity.collectable;
-
-public interface PelletComponent extends Collectable {
-    int getPoints();
-    void collect();
-    void reset();
-    boolean isCollectable();
-    boolean canPassThrough();
-}
